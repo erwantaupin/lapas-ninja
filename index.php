bd0b9ed2adcaf40b03c4fa9040c90605eebb8abb
mikaelhen <mikaelhen28@gmail.com>
2022-02-15 08:28:00 +0100

    8h27

---
+++ index.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+       <header>
+           <div class="logo"><img src="logo.png"></div>
+           <div class="text-right">
+               <a href="#" class="text-white">Get Early Access</a>
+            </div>      
+       </header> 
+
+       <h1>A New Way
+        <br>To Start Business </h1>
+
+        <p>Lorem ipsum dolor sit amet, id nec enim autem oblique, ei dico mentitum duo.
+        <br>Illum iusto laoreet his te. Lorem partiendo mel ex. Ad vitae admodum voluptatum per.</p> 
+        
+        <div class="button">
+            <button type="button">Get Started</button>
+        </div>
+    </div>
+ 
+
+</body>
+</html>
