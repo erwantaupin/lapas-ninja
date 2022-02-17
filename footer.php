e75394ec8cd678118657c0de830d3e905f4d4563
mikaelhen <mikaelhen28@gmail.com>
2022-02-17 10:13:15 +0100

    10h13

---
+++ footer.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/f0fc4e252c.js" crossorigin="anonymous"></script>
+   
+    <title>footer</title>
+</head>
+<body>
+    <footer>
+        <div class="container1">
+            <div class="text-left"><p>Copyright © 2022 Joannie Erwan Mikaël</p></div>
+        </div>
+
+        <div class="res-soc">
+            <div class="facebook">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+            <i class="fa fa-linkedin" aria-hidden="true"></i>
+            <i class="fa fa-medium" aria-hidden="true"></i>
+            
+        </div>
+    </footer>
+    
+</body>
+</html>
+
