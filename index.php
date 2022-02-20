e5a9d0bc5428650d9c3987199882c9bc58977688
erwantaupin <taupin.erwan@laposte.net>
2022-02-20 18:13:33 +0100

    final commit

--- index.php
+++ index.php
@@ -3,13 +3,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" href="./assets/img/favicon.png" sizes="32x32" type="image/png"/>
+    <script src="https://kit.fontawesome.com/f0fc4e252c.js" crossorigin="anonymous"></script>
+    <title>Lapas-ninja</title>
 </head>
 <body>
+    <!-- partie de mika -->
     <div class="container">
        <header>
-           <div class="logo"><img src="logo.png"></div>
+           <div class="logo"><img src="assets/img/logo.png"></div>
            <div class="text-right">
                <a href="#" class="text-white">Get Early Access</a>
             </div>      
@@ -25,7 +28,164 @@
             <button type="button">Get Started</button>
         </div>
     </div>
- 
+    <!-- partie de joannie -->
+    <div class="containero">
+    <h1>Smartest protection for your site</h1>
+    <div class="ensembleicone">
+    <div class="icone1"><img src="./assets/img/smart-protect-1.jpg">
+        <h4>Anti-spam</h4>
+        <p>Lorem ipsum dolor sit amet porro his no his deleniti</p>
+    </div>
+    <div class="icone2"><img src="./assets/img/smart-protect-2.jpg">
+        <h4>Phishing Detect</h4>
+        <p>Ne error antiopam usu. Sed vocen concludaturque ea</p>
+    </div>
+    <div class="icone3"><img src="./assets/img/smart-protect-3.jpg">
+        <h4>Smart Scan</h4>
+        <p>Et usu ocurreret elaboraret doctus prodesse assueverit.</p>
+    </div>
+    </div>
+    <div class="description1">
+        <div class="discours1">
+    <h1>Take a look inside</h1>
+    <p>Te iisque labitur eos, nec sale argumentum scribentur no,
+    augue disputando in vim. Erat fugit sit at, ius lorem deserunt deterruisset no.
+    </p>
+    <div class="tourbouton"> <a href="https://www.lapa.ninja/lab/atlas/#" class="bouton" >Learn more</a></div>   
+    </div>
+    <div class="imagedroite"><img src="./assets/img/feature-1.png"></div>
+    </div>
+    <div class="description2">
+    <div class="imagegauche"><img src="./assets/img/feature-2.png"></div>
+    <div class="discours2">
+    <h1>Safe and reliable</h1>
+    <p> Duo suas detracto maiestatis ad, commodo lucilius invenire nec ad,
+    eum et oratio disputationi. Falli lobortis his ad</p>    
+    <div class="tourbouton">  <a href="https://www.lapa.ninja/lab/atlas/#" class="bouton">Learn more</a></div>
+    </div>
+    </div>
+    </div>
+    <!-- partie de erwan -->
+    <div class="entoureboxprice">
+      <div class="boxprice">
+        <div class="price">
+          <div class="titreprice"><h2>Check our pricing</h2></div>
+          <div class="boxprix">
+            <div class="prix1" id="one">
+              <h4 class="my-4">STARTUP</h4>
+              <p>$<span>0</span>/MO.</p>
+              <li>Up to 5 Documents</li>
+              <li>Up to 3 Reviews</li>
+              <li>5 team Members</li>
+              <li>Limited Support</li>
+              <div class="buttonprix">
+                <a href="http://" class="buttonext">Get Free</a>
+              </div>
+            </div>
+            <div class="prix2" id="two">
+              <h4 class="my-4">PRENIUM</h4>
+              <p>$<span>10</span>/MO.</p>
+              <li>Up to 15 Documents</li>
+              <li>Up to 10 Reviews</li>
+              <li>25 team Members</li>
+              <li>Limited Support</li>
+              <div class="buttonprix">
+                <a href="http://" class="buttoncent">Get Free</a>
+              </div>
+            </div>
+            <div class="prix3" id="three">
+              <h4 class="my-4">PROFESSIONNAL</h4>
+              <p>$<span>30</span>/MO.</p>
+              <li>Unlimited Documents</li>
+              <li>Unlimited Reviews</li>
+              <li>Unlimited Members</li>
+              <li>Unlimited Support</li>
+              <div class="buttonprix">
+                <a href="http://" class="buttonext">Get Free</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="compagny">
+      <div class="boxclient">
+        <div class="client">
+          <div class="padding">
+            <img src="assets/img/client-1.png" class="imgcomp" id="imgc1" />
+          </div>
+          <div class="padding">
+            <img src="assets/img/client-2.png" class="imgcomp" id="imgc2" />
+          </div>
+          <div class="padding">
+            <img src="assets/img/client-3.png" class="imgcomp" id="imgc3" />
+          </div>
+          <div class="padding">
+            <img src="assets/img/client-4.png" class="imgcomp" id="imgc4" />
+          </div>
+          <div class="padding">
+            <img src="assets/img/client-5.png" class="imgcomp" id="imgc5" />
+          </div>
+          <div class="padding">
+            <img src="assets/img/client-6.png" class="imgcomp" id="imgc6" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- partie de joannie -->
+    <div class="containero">
+      <div class="contact">
+        <div class="contactgauche">
+          <h1>Contact Us</h1>
+          <p>
+            Te iisque labitur eos, nec sale argumentum scribentur,<br />
+            augue disputando in vim. Erat fugit sit at, ius lorem.<br /><br />
 
+            Email : company_email@com<br />
+            Phone : 361-688-5824<br />
+            Address : 4826 White Avenue, Corpus Christi, Texas
+          </p>
+          <br />
+        </div>
+
+        <div class="contactdroite">
+          <form>
+            <div class="namemail">
+              <div class="name">
+                <label>Your Name</label><br />
+                <input type="text" />
+              </div>
+
+              <div class="mail">
+                <label>Your Email</label><br />
+                <input type="email" />
+              </div>
+            </div>
+
+            <div class="message">
+              <label>Message</label><br />
+              <textarea> </textarea>
+              <br />
+
+              <input type="submit" value="Submit" class="boutonsubmit" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <!-- partie de mika -->
+    <footer>
+        <div class="container1">
+            <div class="text-left"><p>Copyright © 2022 Joannie Erwan Mikaël</p></div>
+        </div>
+
+        <div class="res-soc">
+            <div class="facebook">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+            <i class="fa fa-linkedin" aria-hidden="true"></i>
+            <i class="fa fa-medium" aria-hidden="true"></i>
+        </div>
+    </footer>
 </body>
 </html>
